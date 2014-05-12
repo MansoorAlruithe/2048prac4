9a0bc0263b4390b493ca51d138249a51cdf208d2
mans <mans@mans-VAIO>
2014-05-13 05:44:51 +0930

    - add testTiltUp() method to test tiltUp() method logic.

--- test/JUnit.java
+++ test/JUnit.java
@@ -88,6 +88,35 @@ public class JUnit {
         Assert.assertEquals(Game.data[3][3], 1);
         
     }
+    
+    @Test
+    public void testTiltUp(){
+        Game.data = new int[][]{{0,0,0,1},
+                                {1,1,1,0},
+                                {1,0,0,1},
+                                {1,0,0,0}};
+        Game.tiltUp();
+        Assert.assertEquals(Game.data[0][0], 1);
+        Assert.assertEquals(Game.data[0][1], 1);
+        Assert.assertEquals(Game.data[0][2], 1);
+        Assert.assertEquals(Game.data[0][3], 1);
+        
+        Assert.assertEquals(Game.data[1][0], 1);
+        Assert.assertEquals(Game.data[1][1], 0);
+        Assert.assertEquals(Game.data[1][2], 0);
+        Assert.assertEquals(Game.data[1][3], 1);
+        
+        Assert.assertEquals(Game.data[2][0], 1);
+        Assert.assertEquals(Game.data[2][1], 0);
+        Assert.assertEquals(Game.data[2][2], 0);
+        Assert.assertEquals(Game.data[2][3], 0);
+        
+        Assert.assertEquals(Game.data[3][0], 0);
+        Assert.assertEquals(Game.data[3][1], 0);
+        Assert.assertEquals(Game.data[3][2], 0);
+        Assert.assertEquals(Game.data[3][3], 0);
+        
+    }
 
 }
 
