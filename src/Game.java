3bd17648560ae6ed539654cdfad6955141345956
mans <mans@mans-VAIO>
2014-05-13 04:06:56 +0930

    - add displayBoard() method to display empty board - add readUserInput() method that reads input from user

--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,33 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author mans
- */
+import java.util.Scanner;
+
+
 public class Game {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static Scanner scan = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        String userInput;
+        do{
+            displayBoard();
+            System.out.print("Next move? 'Q to quit' : ");
+            userInput = readUserInput();
+        }while(!userInput.equalsIgnoreCase("Q"));
+    }
+    
+    public static void displayBoard(){
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                System.out.print("[ ]");
+                if(j==3){
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+    
+    public static String readUserInput(){
+        String userInput = scan.nextLine();
+        return userInput;
     }
 }
