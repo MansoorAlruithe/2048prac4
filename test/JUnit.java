5ba48b6e9552e590fc6ce696fb2e827b2995f21d
mans <mans@mans-VAIO>
2014-05-13 05:52:07 +0930

    - add testTiltDown() method to test tiltDown() method logic

--- test/JUnit.java
+++ test/JUnit.java
@@ -117,6 +117,35 @@ public class JUnit {
         Assert.assertEquals(Game.data[3][3], 0);
         
     }
+    
+    @Test
+    public void testTiltDown(){
+        Game.data = new int[][]{{0,0,0,1},
+                                {1,1,1,0},
+                                {1,0,0,1},
+                                {1,0,0,0}};
+        Game.tiltDown();
+        Assert.assertEquals(Game.data[0][0], 0);
+        Assert.assertEquals(Game.data[0][1], 0);
+        Assert.assertEquals(Game.data[0][2], 0);
+        Assert.assertEquals(Game.data[0][3], 0);
+        
+        Assert.assertEquals(Game.data[1][0], 1);
+        Assert.assertEquals(Game.data[1][1], 0);
+        Assert.assertEquals(Game.data[1][2], 0);
+        Assert.assertEquals(Game.data[1][3], 0);
+        
+        Assert.assertEquals(Game.data[2][0], 1);
+        Assert.assertEquals(Game.data[2][1], 0);
+        Assert.assertEquals(Game.data[2][2], 0);
+        Assert.assertEquals(Game.data[2][3], 1);
+        
+        Assert.assertEquals(Game.data[3][0], 1);
+        Assert.assertEquals(Game.data[3][1], 1);
+        Assert.assertEquals(Game.data[3][2], 1);
+        Assert.assertEquals(Game.data[3][3], 1);
+        
+    }
 
 }
 
